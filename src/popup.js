aacb17aa5f7e8aac7a57214190ea0a73559eee77
Thomas Nowicki <thomas.nowicki@viadee.de>
2020-02-14 18:28:27 +0100

    Added Storage Variable to form submit

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,9 @@
-( () => {
-  'use strict';
-  console = chrome.extension.getBackgroundPage().console;
-  console.log("HELLO WHATSUP")
-} )();
+(() => {
+  "use strict";
+  let paypalForm = document.getElementById("form");
+  let amountField = document.getElementById("paypalFormValueAmount");
+  chrome.storage.sync.get(["savingsAmount"], function(result) {
+    amountField.value = result.savingsAmount;
+  });
+  console.log("running content.js");
+})();
