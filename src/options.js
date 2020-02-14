b77a2af71fd418d3877e50a71d5794275a99718e
Thomas Nowicki <thomas.nowicki@viadee.de>
2020-02-14 16:49:19 +0100

    Nicer Slider Value

--- src/options.js
+++ src/options.js
@@ -1,13 +1,16 @@
-() => {
+(() => {
   "use strict";
 
   console.log("hello from setup.js");
   var slider = document.getElementById("myRange");
-  var output = document.getElementById("demo");
+  var output = document.getElementById("rangeValue");
   output.innerHTML = slider.value; // Display the default slider value
 
   // Update the current slider value (each time you drag the slider handle)
   slider.oninput = function() {
     output.innerHTML = this.value;
+    chrome.storage.sync.set({ savingsAmount: this.value }, function() {
+      console.log("Data Saved");
+    });
   };
-};
+})();
