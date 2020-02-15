2ad0c158898c905feb3f6bc79c85b8695f59f663
Uwe <u.bernitt@eyeo.com>
2020-02-15 06:09:06 +0100

    * Resolved a Merge conflict and * added a settings link to the popup

--- src/popup.js
+++ src/popup.js
@@ -1,17 +1,10 @@
-<<<<<<< Updated upstream
-(() => {
-  "use strict";
-  let paypalForm = document.getElementById("form");
-  let amountField = document.getElementById("paypalFormValueAmount");
-  chrome.storage.sync.get(["savingsAmount"], function(result) {
-    amountField.value = result.savingsAmount;
-  });
-  console.log("running content.js");
-})();
-=======
 ( () => {
   'use strict';
-  console = chrome.extension.getBackgroundPage().console;
-  console.log("HELLO WHATSUP");
+
+  document.querySelector( '#settings' ).addEventListener( 'click', ( e ) => {
+    chrome.tabs.create({ url: "/options.html" } );
+  } );
+  
+  // console = chrome.extension.getBackgroundPage().console;
+  // console.log("HELLO WHATSUP");
 } )();
->>>>>>> Stashed changes
