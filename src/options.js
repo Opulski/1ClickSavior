cf1992c338850c9e171f5c5931cb78c9668e6b3a
Hartog de Mik <hartog.de.mik@gmail.com>
2020-02-15 12:52:25 +0100

    Apply user experience and design to the onboarding.
    Fix css, refactor javascript and html

--- src/options.js
+++ src/options.js
@@ -1,19 +1,24 @@
 (() => {
   "use strict";
 
-  let baseAmount = 10;
+  const baseAmount = 10,
+              PERC = 1,
+               FIX = 2;
 
-  var slider = document.getElementById("myRange");
-  var output = document.getElementById("rangeValue");
+  var  slider = document.getElementById("myRange");
+  var  output = document.getElementById("rangeValue");
   var message = document.getElementById("message");
-  var later = document.getElementById("later");
-  var step2 = document.getElementById("toStep2");
-  var step3 = document.getElementById("toStep3");
-  var step4a = document.getElementById("toStep4a");
-  var step4b = document.getElementById("toStep4b");
- 
+  var   later = document.getElementById("later");
+  var   step2 = document.getElementById("toStep2");
+  var   step3 = document.getElementById("toStep3");
+  var  step4a = document.getElementById("toStep4a");
+  var  step4b = document.getElementById("toStep4b");
 
-  chrome.storage.sync.set({ savingsAmount: baseAmount }, function() {
+  let setKiller = (id) => {
+    document.getElementById(id).onclick = function() { window.close(); };
+  };
+
+  chrome.storage.sync.set({ savingsAmount: baseAmount, savingsType: PERC }, function() {
     slider.value = baseAmount;
     output.innerHTML = parseInt(slider.value); // Display the default slider value
   });
@@ -33,11 +38,25 @@
   step4a.onclick = function() {
     document.querySelector( '#step3' ).style.display = 'none';
     document.querySelector( '#step4a' ).style.display = 'block';
+    setKiller("exit4a");
   }
 
   step4b.onclick = function() {
     document.querySelector( '#step3' ).style.display = 'none';
     document.querySelector( '#step4b' ).style.display = 'block';
+
+    var fixed  = document.getElementById("FestBetrag");
+
+    fixed.oninput = function() {
+      let val = this.value;
+      chrome.storage.sync.set({ savingsAmount: parseInt(val), savingsType: FIX }, function() {
+        setKiller("exit4b");
+        var e4b = document.querySelector("#exit4b");
+        val !== '' ?
+          e4b.removeAttribute("disabled") :
+          e4b.setAttribute("disabled", true);
+      });
+    };
   }
 
   // Update the current slider value (each time you drag the slider handle)
@@ -46,12 +65,8 @@
   };
 
   slider.onmouseup = function() {
-    chrome.storage.sync.set({ savingsAmount: parseInt(this.value) }, function() {
-      message.innerHTML = "<strong>Einstellungen gespeichert.</strong> " +
-        "Du kannst diesen Tab jetzt <a id='killme' href='#'>schließen.</a>";
-
-      var killme = document.getElementById("killme");
-      killme.onclick = function() { window.close(); };
+    chrome.storage.sync.set({ savingsAmount: parseInt(this.value), savingsType: PERC }, function() {
+      return true;
     });
   };
 
