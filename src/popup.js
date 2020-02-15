1a9c22d6182c6dd271687c13bec30881627ff810
Thomas Nowicki <thomas.nowicki@viadee.de>
2020-02-15 08:26:44 +0100

    Popup Progress Bar Implemented!

--- src/popup.js
+++ src/popup.js
@@ -1,16 +1,18 @@
-( () => {
-  'use strict';
+(() => {
+  "use strict";
 
-  document.querySelector( '#settings' ).addEventListener( 'click', ( e ) => {
-    chrome.tabs.create({ url: "/options.html" } );
-  } );
+  document.querySelector("#settings").addEventListener("click", e => {
+    chrome.tabs.create({ url: "/options.html" });
+  });
 
-  chrome.storage.sync.get( [ 'overAllDonations' ], ( result ) => {
+  chrome.storage.sync.get(["overAllDonations"], result => {
     let overAllDonations = result.overAllDonations || 0;
+    var childProgressBar = document.getElementById("childProgressBar");
+    childProgressBar.value = 80;
+    document.querySelector("#overallDonations").innerText =
+      overAllDonations + " EUR";
+  });
 
-    document.querySelector( '#overallDonations' ).innerText = overAllDonations + ' EUR';
-  } );
-  
   // console = chrome.extension.getBackgroundPage().console;
   // console.log("HELLO WHATSUP");
-} )();
+})();
